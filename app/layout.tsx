ef407a0c47fd07a230ae227392e86668d0c9df51
TENKORANG GODFRED <godtenkorang72@gmail.com>
2026-01-15 04:50:48 +0000

    Update root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { TopNav } from "@/components/nav/top-nav";
 
